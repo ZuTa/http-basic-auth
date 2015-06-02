8c600f7a06325c746c537fe42b81dabe32ba2ff6
Taras Zubyk <zuta.developer@gmail.com>
2015-06-02 16:44:54 +0100

    use app.js

--- app.js
+++ app.js
@@ -11,10 +11,11 @@ var basic = auth.basic({
 );
 
 var app = express();
+app.set('port', (process.env.PORT || 5000));
 app.get('/', function(req, res) {
   res.end("Welcome to private area - " + req.user + "!");
 });
 
-http.createServer(basic, app).listen(1337, function() {
-  console.log('Server running at http://127.0.0.1:1337/');
+http.createServer(basic, app).listen(app.get('port'), function() {
+  console.log('Server running at http://127.0.0.1:' + app.get('port'));
 });
